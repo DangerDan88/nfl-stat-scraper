3e54b31fe834eb122650f0cfac065f9eef16af3e
Danger <51889207+DangerDan88@users.noreply.github.com>
2021-01-08 19:28:06 -0700

    working thru extracting data may need to change website data is nested pretty good might not be there on load time for cheerio

--- linkScraper.js
+++ linkScraper.js
@@ -6,7 +6,20 @@ axios(url)
   .then((response) => {
     const data = response.data;
     const $ = cheerio.load(data);
-    const scoringLeaderTable = $("tbody > tr");
-    console.log(scoringLeaderTable.length);
+    const scoringLeaderTable = $(`<tbody> > tr`);
+    const topRushLeaders2020 = [];
+    for (i = 0; scoringLeaderTable.length > i; i++) {
+      console.log(scoringLeaderTable.data);
+    }
+
+    // scoringLeaderTable.each(function () {
+    //   const rushingLeaders = $(this).find("td").text();
+    // console.log(rushingLeaders);
+    //   topRushLeaders2020.push({
+    //     rushingLeaders,
+    //   });
+    //});
+
+    //console.log(topRushLeaders2020);
   })
   .catch(console.error);
